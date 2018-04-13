a74ea41e6f179c865709864fc06c6ff249a254fd
Stephen Carrow <scarrow22@gmail.com>
2018-04-12 23:10:02 -0400

    fixed evaluators directory

---
+++ evaluators/pycocotools/coco_video.py
@@ -0,0 +1,185 @@
+__author__ = 'tylin'
+__version__ = '1.0.1'
+# Interface for accessing the Microsoft COCO dataset.
+
+# Microsoft COCO is a large image dataset designed for object detection,
+# segmentation, and caption generation. pycocotools is a Python API that
+# assists in loading, parsing and visualizing the annotations in COCO.
+# Please visit http://mscoco.org/ for more information on COCO, including
+# for the data, paper, and tutorials. The exact format of the annotations
+# is also described on the COCO website. For example usage of the pycocotools
+# please see pycocotools_demo.ipynb. In addition to this API, please download both
+# the COCO images and annotations in order to run the demo.
+
+# An alternative to using the API is to load the annotations directly
+# into Python dictionary
+# Using the API provides additional utility functions. Note that this API
+# supports both *instance* and *caption* annotations. In the case of
+# captions not all functions are defined (e.g. categories are undefined).
+
+# The following API functions are defined:
+#  COCO       - COCO api class that loads COCO annotation file and prepare data structures.
+#  decodeMask - Decode binary mask M encoded via run-length encoding.
+#  encodeMask - Encode binary mask M using run-length encoding.
+#  getAnnIds  - Get ann ids that satisfy given filter conditions.
+#  getCatIds  - Get cat ids that satisfy given filter conditions.
+#  getImgIds  - Get img ids that satisfy given filter conditions.
+#  loadAnns   - Load anns with the specified ids.
+#  loadCats   - Load cats with the specified ids.
+#  loadImgs   - Load imgs with the specified ids.
+#  segToMask  - Convert polygon segmentation to binary mask.
+#  showAnns   - Display the specified annotations.
+#  loadRes    - Load result file and create result api object.
+# Throughout the API "ann"=annotation, "cat"=category, and "img"=image.
+# Help on each functions can be accessed by: "help COCO>function".
+
+# See also COCO>decodeMask,
+# COCO>encodeMask, COCO>getAnnIds, COCO>getCatIds,
+# COCO>getImgIds, COCO>loadAnns, COCO>loadCats,
+# COCO>loadImgs, COCO>segToMask, COCO>showAnns
+
+# Microsoft COCO Toolbox.      Version 1.0
+# Data, paper, and tutorials available at:  http://mscoco.org/
+# Code written by Piotr Dollar and Tsung-Yi Lin, 2014.
+# Licensed under the Simplified BSD License [see bsd.txt]
+
+import json
+import datetime
+import matplotlib.pyplot as plt
+from matplotlib.collections import PatchCollection
+from matplotlib.patches import Polygon
+import numpy as np
+from skimage.draw import polygon
+import copy
+
+class COCO:
+    def __init__(self, annotation_file=None):
+        """
+        Constructor of Microsoft COCO helper class for reading and visualizing annotations.
+        :param annotation_file (str): location of annotation file
+        :param image_folder (str): location to the folder that hosts images.
+        :return:
+        """
+        # load dataset
+        self.dataset = {}
+        self.anns = []
+        self.imgToAnns = {}
+        self.catToImgs = {}
+        self.imgs = []
+        self.cats = []
+        if not annotation_file == None:
+            print('loading annotations into memory...')
+            time_t = datetime.datetime.utcnow()
+            dataset = json.load(open(annotation_file, 'r'))
+            print(datetime.datetime.utcnow() - time_t)
+            self.dataset = dataset
+            self.createIndex()
+
+    def createIndex(self):
+        # create index
+        print('creating index...')
+        imgToAnns = {int(video['videoid'][5:]): [] for video in self.dataset['data']}
+        for video in self.dataset['data']:
+            for caption in video['captions']:
+                imgToAnns[int(video['videoid'][5:])] += [{'caption': ' '.join(caption).encode('utf-8').strip()}]
+
+        print('index created!')
+
+        # create class members
+        self.imgToAnns = imgToAnns
+
+    def getImgIds(self, imgIds=[], catIds=[]):
+        '''
+        Get img ids that satisfy given filter conditions.
+        :param imgIds (int array) : get imgs for given ids
+        :param catIds (int array) : get imgs with all given cats
+        :return: ids (int array)  : integer array of img ids
+        '''
+
+        ids = self.imgToAnns.keys()
+
+        return list(ids)
+
+    def loadRes(self, resFile):
+        """
+        Load result file and return a result api object.
+        :param   resFile (str)     : file name of result file
+        :return: res (obj)         : result api object
+        """
+        res = COCO()
+
+        print('Loading and preparing results...     ')
+        time_t = datetime.datetime.utcnow()
+        anns = json.load(open(resFile, 'r'))
+        assert type(anns) == list, 'results in not an array of objects'
+        annsImgIds = [int(ann['videoid'][5:]) for ann in anns]
+        assert set(annsImgIds) == (set(annsImgIds) & set(self.getImgIds())), \
+               'Results do not correspond to current test set'
+
+        res.dataset['data'] = anns
+        res.createIndex()
+        return res
+
+
+    @staticmethod
+    def decodeMask(R):
+        """
+        Decode binary mask M encoded via run-length encoding.
+        :param   R (object RLE)    : run-length encoding of binary mask
+        :return: M (bool 2D array) : decoded binary mask
+        """
+        N = len(R['counts'])
+        M = np.zeros( (R['size'][0]*R['size'][1], ))
+        n = 0
+        val = 1
+        for pos in range(N):
+            val = not val
+            for c in range(R['counts'][pos]):
+                R['counts'][pos]
+                M[n] = val
+                n += 1
+        return M.reshape((R['size']), order='F')
+
+    @staticmethod
+    def encodeMask(M):
+        """
+        Encode binary mask M using run-length encoding.
+        :param   M (bool 2D array)  : binary mask to encode
+        :return: R (object RLE)     : run-length encoding of binary mask
+        """
+        [h, w] = M.shape
+        M = M.flatten(order='F')
+        N = len(M)
+        counts_list = []
+        pos = 0
+        # counts
+        counts_list.append(1)
+        diffs = np.logical_xor(M[0:N-1], M[1:N])
+        for diff in diffs:
+            if diff:
+                pos +=1
+                counts_list.append(1)
+            else:
+                counts_list[pos] += 1
+        # if array starts from 1. start with 0 counts for 0
+        if M[0] == 1:
+            counts_list = [0] + counts_list
+        return {'size':      [h, w],
+               'counts':    counts_list ,
+               }
+
+    @staticmethod
+    def segToMask( S, h, w ):
+         """
+         Convert polygon segmentation to binary mask.
+         :param   S (float array)   : polygon segmentation mask
+         :param   h (int)           : target mask height
+         :param   w (int)           : target mask width
+         :return: M (bool 2D array) : binary mask
+         """
+         M = np.zeros((h,w), dtype=np.bool)
+         for s in S:
+             N = len(s)
+             rr, cc = polygon(np.array(s[1:N:2]), np.array(s[0:N:2])) # (y, x)
+             M[rr, cc] = 1
+         return M
